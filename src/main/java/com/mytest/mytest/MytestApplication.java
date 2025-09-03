677554ad0cb3d3c8626f2d54094bcd9fd968cf80
yush <yush2202@gmail.com>
2025-09-03 14:22:56 +0900

    feat: cd 확인

--- src/main/java/com/mytest/mytest/MytestApplication.java
+++ src/main/java/com/mytest/mytest/MytestApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MytestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MytestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MytestApplication.class, args); //
+    }
 
 }
